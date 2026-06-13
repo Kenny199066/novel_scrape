a7af56632a62b687c3fbf473980af7387b335665
Kenny199066 <murigi.km@gmail.com>
2026-06-13 13:04:22 +0300

    add beautifulSoup,scrape, clean, extract and store the scraped books in a list

--- bookscrape.py
+++ bookscrape.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
+
 url = "https://books.toscrape.com/index.html"
 
 response = requests.get(url)
@@ -7,6 +9,22 @@ print(response.status_code)
 
 #print(response.content)
 
+soup = BeautifulSoup(response.content, 'html.parser')
 
+cards = soup.select('.product_pod')
+#print(cards[0])
+all_scraped_books = []
+for card in cards:
+    title = card.select_one("h3 a").text
+    price = card.select_one(".price_color").text
 
+    #print(title, price)
 
+    book_item = {
+            "title": title,
+            "price": price
+    } 
+
+    all_scraped_books.append(book_item)
+
+print(all_scraped_books)
